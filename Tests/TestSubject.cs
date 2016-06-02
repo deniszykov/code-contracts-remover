d52c4fa3e36d71e8d88dd3f24a456843f830efbe
shai_hulud <deniszykov@gmail.com>
2016-06-02 11:30:56 +0300

    added test subject

---
+++ Tests/TestSubject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+	public static class Utils
+	{
+		public static Task FaultedTask(Exception error)
+		{
+			Contract.Requires<ArgumentNullException>(error != null);
+			Contract.Ensures(Contract.Result<Task>() != null);
+
+			return FaultedTask<object>(error);
+		}
+	}
+}
